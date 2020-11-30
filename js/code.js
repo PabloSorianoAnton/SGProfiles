b965894a35445d4eb530d211c44790f93398857b
Pablo Soriano Antón <10006712.joan23@fje.edu>
2020-11-30 20:46:53 +0100

    Primera subida 30/11/2020

---
+++ js/code.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    modal = document.getElementById('myModal');
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
---
+++ model/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define ("BD","bd_gallery");
+define ("SERVIDOR","localhost");
+define ("USER","root");
+define ("PASSWORD","");
+
+?>
